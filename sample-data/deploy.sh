38ca06d985492e043ffeccf33a8f2d67874428a9
Aditya Pandita <72813685+adityapandita97@users.noreply.github.com>
2026-02-13 11:56:22 +0530

    Update and rename deploy.sh to meter.csv

--- sample-data/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Automated deployment script
-aws cloudformation create-stack \
-  --stack-name redshift-quicksight-pipeline \
-  --template-body file://cloudformation-template.yaml \
-  --parameters file://parameters.json \
-  --capabilities CAPABILITY_NAMED_IAM
